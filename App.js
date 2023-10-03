147081a6ff5f955963d27eebf03cf214bbb64d3f
deltaryss <solarylord@gmail.com>
2023-10-03 23:52:14 +0200

    init

---
+++ App.js
@@ -0,0 +1,4 @@
+//imports
+const config = require('./config.js');
+
+//initialize express
---
+++ conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    database: {
+        host: '', // Adresse du serveur; String
+        user: 'dev', // Utilisateur; String
+        password: 'm9L4#83WzY=m%f', // Mot de passe; String
+        database: 'dev', // Nom de la base de données; String
+        port: 3306, // Port du serveur; Number
+    },
+    port: 4523, // Port de l'API; Number
+    createTable: false, // Possibilitée de créer la table si elle n'existe pas; Boolean
+    debug: false, // Affiche les logs dans la console; Boolean
+};
